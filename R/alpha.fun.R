ac6f3293538119e26d4dca8b645146ed752db074
unknown <z3103495@MATHT292.adns.unsw.edu.au>
2013-06-03 12:18:25 +1000

    correction to alpha.fun function

--- R/alpha.fun.R
+++ R/alpha.fun.R
@@ -5,12 +5,12 @@ alpha.fun<-function(r,k,q)
 
   c<-sqrt(c)
   eta<-NULL
-  eta[r<=c]<-(r[r^2<=c^2])^2/(2*sig^2) 
-  eta[r>c]<-c^2/(4*sig^2) 
+  eta[r<=c]<-(r[r^2<=c^2])^2/(k*sig) 
+  eta[r>c]<-c^2/( (k+2)*sig ) 
   eta <- mean(eta)
 
   alpha<-NULL
-  alpha[r<=c]<-(r[r<=c])^2/(eta*sig^2)
-  alpha[r>c]<-c^2/(eta*sig^2)
+  alpha[r<=c]<-(r[r<=c])^2/(eta*sig)
+  alpha[r>c]<-c^2/(eta*sig)
   alpha
 }
