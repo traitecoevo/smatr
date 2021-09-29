95fe9ef5d60ad68d1b1acd5bee8991bfeb105227
Fonti Kar <f.kar@unsw.edu.au>
2021-09-29 16:57:05 +1000

    Added V = V in L169, tests passing addresses a simple case for #6

--- R/sma.R
+++ R/sma.R
@@ -5,7 +5,8 @@ sma <- function(formula, data, subset, na.action, log='',
 	 robust=FALSE,V=matrix(0,2,2),n_min=3,quiet=FALSE,
 	 ...)
 {
-	method <- match.arg(method)
+ 
+  method <- match.arg(method)
 	type <- match.arg(type)
 	multcompmethod <- match.arg(multcompmethod)
 	
@@ -165,7 +166,7 @@ sma <- function(formula, data, subset, na.action, log='',
 	
 		#sma coefficients 
 		coeff[[i]] <- line.cis(Y,X,intercept=intercept, method=method, 
-			alpha=alpha, robust=robust, ...)   
+			alpha=alpha, robust=robust, V=V, ...)   
 
 		# correlation and P-value
 		if(intercept){
--- tests/testthat/test-measurement_error.R
+++ tests/testthat/test-measurement_error.R
@@ -1,17 +1,16 @@
 data(leaflife)
-leaf.low.soilp <-leaflife %>% filter(soilp == 'low')
+leaf.low.soilp <-leaflife %>% dplyr::filter(soilp == 'low')
 V2 = matrix(c(0.01,0,0.0,0.01),2,2)
 
 #Most simple fit
 default_sma <- sma(longev~lma, data=leaf.low.soilp)
-default_sma$coef
-
 measure_error_sma <- sma(longev~lma, data=leaf.low.soilp, V = V2)
 
 test_that("Functions accounts for measurement error", {
+  expect_silent(sma(longev~lma, data=leaf.low.soilp, V = V2))
   expect_true(measure_error_sma$coef[[1]][1,1] != default_sma$coef[[1]][1,1])
   expect_true(measure_error_sma$coef[[1]][2,1] != default_sma$coef[[1]][2,1])
-  expect_false(all.equal(measure_error_sma$coef, default_sma$coef))
+  expect_vector(all.equal(measure_error_sma$coef, default_sma$coef))
 }) 
 
 
