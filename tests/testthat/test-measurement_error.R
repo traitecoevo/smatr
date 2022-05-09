4ead7fc74b0bebe35d8a76a728393dada7964fdb
Fonti Kar <f.kar@unsw.edu.au>
2022-05-09 11:01:19 +1000

    Adding tests to cross verify with other functions

--- tests/testthat/test-measurement_error.R
+++ tests/testthat/test-measurement_error.R
@@ -20,17 +20,67 @@ V2group = array(c(V2,V2),c(2,2,2)) # in array form
 
 # var(leaf.low.soilp[,c("logLMA","logLongev")]) #True variances
 
-#Most simple fits
+#No group fits
 default_sma <- sma(logLongev~logLMA, data=leaf.low.soilp)
 default_sma$coef 
 
 me_sma <- sma(logLongev~logLMA, data=leaf.low.soilp, V = V2)
 me_sma$coef
 
-test_that("Functions accounts for measurement error", {
+test_that("Single estimations", {
   expect_silent(sma(longev~lma, data=leaf.low.soilp, V = V2))
-  expect_true(abs(me_sma$coef[[1]][1,1]) < abs(default_sma$coef[[1]][1,1]))
-  expect_true(me_sma$coef[[1]][2,1] < default_sma$coef[[1]][2,1])
+  expect_true(abs(me_sma$coef[[1]][2,1]) < abs(default_sma$coef[[1]][2,1]))
+}) 
+
+# Group fits common elevation
+default_gp_sma <- sma(logLongev~logLMA+rain, data=leaf.low.soilp)
+default_gp_sma$coef 
+
+me_gp_sma <- sma(logLongev~logLMA+rain, data=leaf.low.soilp, V = V2)
+me_gp_sma$coef
+
+test_that("Common elevation group estimations", {
+  expect_silent(sma(longev~lma+rain, data=leaf.low.soilp, V = V2))
+  expect_true(me_gp_sma$coef[[1]][2,1] != default_gp_sma$coef[[1]][2,1])
+  expect_true(me_gp_sma$coef[[2]][2,1] != default_gp_sma$coef[[2]][2,1])
+}) 
+
+# Group fits common slope
+default_gp_inter_sma <- sma(logLongev~logLMA*rain, data=leaf.low.soilp)
+default_gp_inter_sma$coef 
+
+me_gp_inter_sma <- sma(logLongev~logLMA*rain, data=leaf.low.soilp, V = V2)
+me_gp_inter_sma$coef
+
+test_that("Common slope group estimations", {
+  expect_silent(sma(longev~lma*rain, data=leaf.low.soilp, V = V2))
+  expect_true(me_gp_inter_sma$coef[[1]][2,1] != default_gp_inter_sma$coef[[1]][2,1])
+  expect_true(me_gp_inter_sma$coef[[2]][2,1] != default_gp_inter_sma$coef[[2]][2,1])
+}) 
+
+# Array V matrices
+arr_default_sma <- sma(logLongev~logLMA+rain, data=leaf.low.soilp)
+arr_default_sma$coef #works
+
+arr_me_sma <- sma(logLongev~logLMA+rain, data=leaf.low.soilp, V = V2group)
+arr_me_sma$coef #works
+
+test_that("Array group estimations", {
+  expect_silent(sma(logLongev~logLMA+rain, data=leaf.low.soilp, V = V2group))
+  expect_length(V2group, length(V2group))
+  expect_true(arr_me_sma$coef[[1]][2,1] != arr_default_sma$coef[[1]][2,1])
+  expect_true(arr_me_sma$coef[[2]][2,1] != arr_default_sma$coef[[2]][2,1])
+}) 
+
+# Cross checking with other functions
+# Create high rainfall, low soil ph dataset
+highrain.lowsoilp <- leaf.low.soilp[leaf.low.soilp$rain == "high",]
+
+test_that("Values match other functions", {
+  expect_equal(me_sma$coef[[1]], 
+               line.cis(leaf.low.soilp$logLongev, leaf.low.soilp$logLMA, V = V2))
+  expect_equal(me_gp_inter_sma$coef$high,
+               line.cis(highrain.lowsoilp$logLongev, highrain.lowsoilp$logLMA, V = V2))
 }) 
 
 
@@ -39,11 +89,3 @@ test_that("Functions accounts for measurement error", {
 
 
 
-
-
-
-
-
-
-arr_me_sma <- sma(logLongev~logLMA+rain, data=leaf.low.soilp, V = V2group)
-arr_me_sma$coef #works
