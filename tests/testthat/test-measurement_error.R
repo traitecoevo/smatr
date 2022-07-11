dfcfc1b00771bda3a87f375b1adffede30fae6a2
Fonti Kar <f.kar@unsw.edu.au>
2022-07-11 10:36:24 +1000

    Added a lot of tests for measurement error

--- tests/testthat/test-measurement_error.R
+++ tests/testthat/test-measurement_error.R
@@ -94,4 +94,70 @@ test_that("Values are different", {
   expect_true(me_gp_inter_sma$coef[[1]][2,1] == coef.sma(me_gp_inter_sma)[1,2])
 }) 
 
+leaflife$logLMA=log10(leaflife$lma) 
+leaflife$logLongev=log10(leaflife$longev) 
 
+#Adding error to the y variable (logLongev)
+ftMeasY=sma(logLongev~logLMA*site,data=leaflife,V=matrix(c(0.01,0,0,0),ncol=2)) 
+ftZero=sma(logLongev~logLMA*site,data=leaflife,V=matrix(c(0,0,0,0),ncol=2)) 
+ftMeasY$coef 
+ftZero$coef 
+#ftMEasY should have flatter slopes than all of ftZero 
+
+expect_lt(ftMeasY$coef$`1`[2,1], ftZero$coef$`1`[2,1])
+expect_lt(ftMeasY$coef$`2`[2,1], ftZero$coef$`2`[2,1])
+expect_lt(ftMeasY$coef$`3`[2,1], ftZero$coef$`3`[2,1])
+expect_lt(ftMeasY$coef$`4`[2,1], ftZero$coef$`4`[2,1])
+
+#Adding error to the x variable (logLMA)
+ftMeasX=sma(logLongev~logLMA*site,data=leaflife,V=matrix(c(0,0,0,0.002),ncol=2)) 
+ftZero=sma(logLongev~logLMA*site,data=leaflife,V=matrix(c(0,0,0,0),ncol=2)) 
+ftMeasX$coef 
+ftZero$coef 
+#ftMEasX should be smaller/less negative
+
+expect_lt(ftMeasX$coef$`1`[1,1], ftZero$coef$`1`[1,1])
+expect_lt(ftMeasX$coef$`2`[1,1], ftZero$coef$`2`[1,1])
+expect_lt(ftMeasX$coef$`3`[1,1], ftZero$coef$`3`[1,1])
+expect_lt(ftMeasX$coef$`4`[1,1], ftZero$coef$`4`[1,1])
+
+
+ft=sma(logLongev~logLMA*site,data=leaflife) 
+ftZero=sma(logLongev~logLMA*site,data=leaflife,V=matrix(c(0,0,0,0),ncol=2)) 
+# ft and ftZero should be identical 
+ft$coef 
+ftZero$coef 
+
+expect_identical(ft$coef$`1`[1,1], ftZero$coef$`1`[1,1])
+expect_identical(ft$coef$`2`[1,1], ftZero$coef$`2`[1,1])
+expect_identical(ft$coef$`3`[1,1], ftZero$coef$`3`[1,1])
+expect_identical(ft$coef$`4`[1,1], ftZero$coef$`4`[1,1])
+expect_identical(ft$coef$`1`[2,1], ftZero$coef$`1`[2,1])
+expect_identical(ft$coef$`2`[2,1], ftZero$coef$`2`[2,1])
+expect_identical(ft$coef$`3`[2,1], ftZero$coef$`3`[2,1])
+expect_identical(ft$coef$`4`[2,1], ftZero$coef$`4`[2,1])
+
+# Fitting a common slope
+
+ftZero=sma(logLongev~logLMA+site,data=leaflife,V=matrix(c(0,0,0,0),ncol=2)) 
+coef(ftZero) 
+ft=sma(logLongev~logLMA+site,data=leaflife) 
+coef(ft)
+ftMeasY=sma(logLongev~logLMA+site,data=leaflife,V=matrix(c(0.001,0,0,0),ncol=2)) 
+coef(ftMeasY) 
+ftMeasX=sma(logLongev~logLMA+site,data=leaflife,V=matrix(c(0,0,0,0.002),ncol=2)) 
+coef(ftMeasX) 
+
+# common slope flatter for ftMeasY, elevs higher, less negative
+expect_lt(coef(ftMeasY)[1,2], coef(ftZero)[1,2])
+expect_gt(coef(ftMeasY)[1,1], coef(ftZero)[1,1])
+expect_gt(coef(ftMeasY)[2,1], coef(ftZero)[2,1])
+expect_gt(coef(ftMeasY)[3,1], coef(ftZero)[3,1])
+expect_gt(coef(ftMeasY)[4,1], coef(ftZero)[4,1])
+
+# common slope steeper for ftMeasX, elevs lower, more negative 
+expect_gt(coef(ftMeasX)[1,2], coef(ftZero)[1,2])
+expect_lt(coef(ftMeasX)[1,1], coef(ftZero)[1,1])
+expect_lt(coef(ftMeasX)[2,1], coef(ftZero)[2,1])
+expect_lt(coef(ftMeasX)[3,1], coef(ftZero)[3,1])
+expect_lt(coef(ftMeasX)[4,1], coef(ftZero)[4,1])
