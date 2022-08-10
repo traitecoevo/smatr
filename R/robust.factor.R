afd9fc9e9fcb2715f4f10f2cc114ae6f74d7686c
Fonti Kar <f.kar@unsw.edu.au>
2022-08-10 13:14:58 +1000

    Reverted changes

--- R/robust.factor.R
+++ R/robust.factor.R
@@ -18,6 +18,23 @@ robust.factor<-function(data,q)
 }
 
 
+alpha.fun<-function(r,k,q) 
+{
+  c<-qchisq(q,k)
+  sig<-pchisq(c,k+2)+(c/k)*(1-q)
+
+  c<-sqrt(c)
+  eta<-NULL
+  eta[r<=c]<-(r[r^2<=c^2])^2/(2*sig^2) 
+  eta[r>c]<-c^2/(4*sig^2) 
+  eta <- mean(eta)
+
+  alpha<-NULL
+  alpha[r<=c]<-(r[r<=c])^2/(eta*sig^2)
+  alpha[r>c]<-c^2/(eta*sig^2)
+  alpha
+}
+
 gamma.fun<-function(r,k,q) 
 {
   c<-sqrt(qchisq(q,k))
