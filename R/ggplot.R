0a24b8450fd4ce4bb489886ef143a2499df65c9f
Fonti Kar <f.kar@unsw.edu.au>
2022-10-19 17:07:59 +1100

    Making group x and y transformed data #1

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,7 +1,7 @@
 #' Title
 #'
 #' @param obj 
-#' @param ... arguments passed to ggplot()
+#' @param ... arguments passed to geom_line()
 #'
 #' @return ggplot2 obj
 #' @export
@@ -74,6 +74,27 @@ ggplot_y <- function(obj, pdat, ...){
   p +
     geom_point(aes(x = X, y = Y_raw, group = group, colour = group), shape = 21, size = 2) + 
     geom_line(...) + 
+    scale_y_log10() + 
+    ylab(label = obj$variables[1]) +
+    xlab(label = obj$variables[2]) +
+    theme_bw()
+}
+
+#' Make ggplot with transformed x and y axes 
+#' @inheritParams ggplot.sma
+
+ggplot_xy <- function(obj, pdat, ...){
+  if(any(obj$groups == "all"))
+    p <- ggplot(data = pdat, aes(x = X, y = line_Y), ...) 
+  
+  if(length(obj$groups) > 1)
+    p <- ggplot(data = pdat, aes(x = X_raw, y = line_Y, group = group, colour = group), ...)
+  
+  # Construct rest of plot
+  p +
+    geom_point(aes(x = X_raw, y = Y_raw, group = group, colour = group), shape = 21, size = 2) + 
+    geom_line(...) + 
+    scale_x_log10() +
     scale_y_log10() + 
     ylab(label = obj$variables[1]) +
     xlab(label = obj$variables[2]) +
@@ -207,11 +228,34 @@ make_plot_data <- function(obj){
  #' @inheritParams make_data_raw
  
  make_data_xy <- function(obj){
+   if(any(obj$groups == "all")){
    dplyr::tibble(X = obj$data[,2], #On log scale
                  Y = obj$data[,1], #On log scale
                  X_raw = 10^X,
+                 Y_raw = 10^Y,
                  line_Y = 10^summary(obj)$Int * X_raw^summary(obj)$Slope # 10^a*x^B
                  )
+   }else{
+     pdat <- dplyr::tibble(X = obj$data[,2], #On log scale
+                           Y = obj$data[,1], #On log scale
+                           X_raw = 10^X,
+                           Y_raw = 10^Y,
+                           group = obj$data[,3])
+     
+     groups <- levels(obj$data[,3])
+     for(i in 1:length(groups)){
+       pdat[pdat$group == groups[i],"a"] <- get_coef(obj = obj, 
+                                                     group_level = groups[i],
+                                                     coef_type = "a")
+       
+       pdat[pdat$group == groups[i],"B"] <- get_coef(obj = obj, 
+                                                     group_level = groups[i],
+                                                     coef_type = "B")
+       
+       pdat <- pdat |> mutate(line_Y = 10^a*X_raw^B) # 10^a*x^B
+     }
+   }
+   pdat  
  }
 
 
